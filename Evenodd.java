13ca9a50de6d5d93fcd3b5db5151886ea29e6d29
pannusimran <39096234+pannusimran@users.noreply.github.com>
2018-10-22 15:35:39 -0400

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,24 @@
+
+package evenodd;
+
+
+public class Evenodd {
+
+    public static void main(String[] args) {
+        int a;
+       for (a=1;a<1025;a++){
+           if(a%2==0){
+                System.out.println("number is even" + a);
+            }
+            else
+            {
+                System.out.println("number is odd" +a);
+            }
+            
+        }
+    
+    }
+}
+
+    
+
